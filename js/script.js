b8476b59c3dfd513c938ac64e10e3e9236609730
vladimirov535 <39128149+vladimirov535@users.noreply.github.com>
2020-04-23 20:07:47 +0200

    24/04

--- js/script.js
+++ js/script.js
@@ -34,32 +34,6 @@
 				copyrightYear:           $( '.copyright-year' ),
 			};
 
-	// Initialize scripts that require a loaded window
-	$window.on('load', function () {
-		// Page loader & Page transition
-		if (plugins.preloader.length && !isNoviBuilder) {
-			pageTransition({
-				target: document.querySelector( '.page' ),
-				delay: 0,
-				duration: 500,
-				classIn: 'fadeIn',
-				classOut: 'fadeOut',
-				classActive: 'animated',
-				conditions: function (event, link) {
-					return link && !/(\#|javascript:void\(0\)|callto:|tel:|mailto:|:\/\/)/.test(link) && !event.currentTarget.hasAttribute('data-lightgallery');
-				},
-				onTransitionStart: function ( options ) {
-					setTimeout( function () {
-						plugins.preloader.removeClass('loaded');
-					}, options.duration * .75 );
-				},
-				onReady: function () {
-					plugins.preloader.addClass('loaded');
-					windowReady = true;
-				}
-			});
-		}
-	});
 
 // Initialize scripts that require a finished document
 	$(function () {
